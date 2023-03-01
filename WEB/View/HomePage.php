2663930ad4534efae137965a29b2a35ce498e56d
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2023-03-01 19:26:01 +0600

    Update HomePage.php

--- WEB/View/HomePage.php
+++ WEB/View/HomePage.php
@@ -2,20 +2,18 @@
 include "../Control/Process.php"
 ?>
 <html>
-    <title>
-            HomePage
-        </title>
+    <title> HomePage </title>
     <body bgcolor="orange">  
     <br><br>
        
         <center><h1>Welcome to Tourist Point</h1> </center>
         <center><h2>Join As</h2></center>
-        <center> <button type="button">Admin</button><br></center>
+        <center> <a href="admin_login.php">Admin</a><br></center>
         <br><br><br>
-        <center> <button type="button" a href="../view/login.php">Employee</button><br></center>
+        <center> <a href="../view/login.php">Employee</a><br></center>
         <br><br><br>
-        <center><button type="button">Customer</button><br></center>
+        <center><a href="#">Customer</a><br></center>
         <br><br><br>
-        <center><button type="button">Tourist Guide</button><br></center>
+        <center><a href="#">Tourist Guide</a><br></center>
     </body>
 </html>
