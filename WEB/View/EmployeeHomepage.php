97edcb23c53332a7d5bdf94615afa154bb3a0ef0
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2023-03-01 21:03:39 +0600

    Update EmployeeHomepage.php

--- WEB/View/EmployeeHomepage.php
+++ WEB/View/EmployeeHomepage.php
@@ -13,13 +13,13 @@ if (empty($_SESSION["username"])) {
 
 <body bgcolor="pink">  
 <center><h1>Welcome Back</h1> </center>
-        <center> <button type="button">View Profile</button><br></center>
+        <center> <a href="view_profile.php">View Profile</a><br></center>
         <br><br><br>
-        <center> <button type="button">Update Data of Guides</button><br></center>
+        <center> <a href="update_guides_data.php">Update Data of Guides</a><br></center>
         <br><br><br>
-        <center><button type="button">Update Data of Customer</button><br></center>
+        <center><a href="update_customer_data.php">Update Data of Customer</a><br></center>
         <br><br><br>
-        <center><button type="button">Tourist Guide</button><br></center><br>
+        <center><a href="tourist_guide.php">Tourist Guide</a><br></center><br>
         <center>  <a href="../control/logout.php">
          <h2><i>Logout</h2></i></a><center>
 </body>
