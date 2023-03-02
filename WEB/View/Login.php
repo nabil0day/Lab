541af32161e113a10fb4caa093df464e54d283bd
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2023-03-02 16:58:33 +0600

    Update Login.php

--- WEB/View/Login.php
+++ WEB/View/Login.php
@@ -1,7 +1,8 @@
 <?php
 include('../control/loginCheck.php');
 if(isset($_SESSION['username'])){
-header("location: EmployeeHomepage.php");
+    header("location: EmployeeHomepage.php");
+    exit();
 }
 ?>
 <html>
@@ -9,44 +10,36 @@ header("location: EmployeeHomepage.php");
 
     </head>
     <body bgcolor="skyblue">  
-        <title>
-        Login
-        </title><br><br><br><br><br><br>
-        <center><h1>Login</h1> </center>
+        <title>Login</title>
+        <br><br><br><br><br><br>
+        <center><h1>Login</h1></center>
         <center>
             <table>
-            <form action="" method="post">
+                <form action="" method="post">
 
-        <tr>
-            <th>User Name: </th>
-            <th><input type="text" name="username" size="20"/> </th>
-        <tr>
-        <tr>
-            <th><b>Password: </b></th>
-            <th><input type="password" name="password" size="20"/> </th>
-        <tr>
-        
-        <tr>
-            <th></th>
-            <th><button>Submit</button></th>
-            <th></th>
-            
-        <tr>
-        <tr>
-            <th></th>
-            <th><button>Reset</button></th>
-            <th></th>
-            
-        <tr>
-        </form>
-        <?php 
-        echo $error; 
-         ?>
-             
-
-        </table>
+                    <tr>
+                        <th>User Name: </th>
+                        <th><input type="text" name="username" size="20"/> </th>
+                    </tr>
+                    <tr>
+                        <th><b>Password: </b></th>
+                        <th><input type="password" name="password" size="20"/> </th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <th><button type="submit" name="submit">Submit</button></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <th><button type="reset">Reset</button></th>
+                        <th></th>
+                    </tr>
+                </form>
+            </table>
         </center>
-        
-       
+        <?php 
+            echo $error; 
+         ?>
     </body>
 </html>
